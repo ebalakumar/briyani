a134906d4df1dbee64414b4e96f5c5e3b6032ac9
Bala <40795470+ebalakumar@users.noreply.github.com>
2023-06-15 20:48:05 +0200

    add ingredient section

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function calculate() {
   // Calculate steps
   var riceQuantity = meatWeight;
   var waterQuantity = meatWeight * 800;
+  var spiceClove = meatWeight * 3;
+  var spiceCardamom = meatWeight * 3;
+  var spiceCinnamon = meatWeight * 3;
 
   // Display the steps
+  document.getElementById('spiceClovePrepare').textContent =  spiceClove + ' numbers. ';
+  document.getElementById('spiceCardamomPrepare').textContent =  spiceCardamom + ' numbers. ';
+  document.getElementById('spiceCinnamonPrepare').textContent =  spiceCinnamon + ' inches. ';
   document.getElementById('riceStep').textContent = 'You need ' + riceQuantity + ' kg of rice.';
   document.getElementById('waterStep').textContent = 'You need to add ' + waterQuantity + ' ml of water.';
 }
