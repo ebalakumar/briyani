41e2e018f769a0fcaa44d9b27d4219907208fc2c
Bala <40795470+ebalakumar@users.noreply.github.com>
2023-06-15 20:37:16 +0200

    first logic to create receipe

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// Get the input values from the form and perform calculations
+// Get the input value from the form and calculate the steps to make a food dish
 function calculate() {
-    // Get input values
-    var number1 = parseFloat(document.getElementById('number1').value);
-    var number2 = parseFloat(document.getElementById('number2').value);
-  
-    // Perform calculations
-    var sum = number1 + number2;
-    var difference = number1 - number2;
-    var product = number1 * number2;
-    var quotient = number1 / number2;
-  
-    // Display the results
-    document.getElementById('sum').textContent = 'Sum: ' + sum;
-    document.getElementById('difference').textContent = 'Difference: ' + difference;
-    document.getElementById('product').textContent = 'Product: ' + product;
-    document.getElementById('quotient').textContent = 'Quotient: ' + quotient;
-  }
-  
+  // Get input value
+  var meatWeight = parseInt(document.getElementById('meatWeight').value);
+
+  // Calculate steps
+  var riceQuantity = meatWeight;
+  var waterQuantity = meatWeight * 800;
+
+  // Display the steps
+  document.getElementById('riceStep').textContent = 'You need ' + riceQuantity + ' kg of rice.';
+  document.getElementById('waterStep').textContent = 'You need to add ' + waterQuantity + ' ml of water.';
+}
